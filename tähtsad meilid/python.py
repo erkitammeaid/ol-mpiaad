9afdcdb27179e26a71849e818350336f9d25ff11
erkitammeaid <erkipoiss01@gmail.com>
2020-10-20 10:23:13 +0300

    tähtsad meilid

--- tähtsad meilid/python.py
+++ tähtsad meilid/python.py
@@ -1,8 +1,16 @@
 counter = 0
 f = open("meilsis.txt", "r")
-first_line = f.readline()
-print(first_line)
+first_line = int(f.readline())
+#print(first_line)
 mails = []
 for i in range(int(first_line)):
     second_line = f.readline()
-    print(second_line)
+    mails.append(second_line)
+    #print(second_line)
+third_line = int(f.readline())
+#print(third_line)
+for h in range(third_line):
+    mails2 = f.readline()
+    if mails2 in mails:
+        counter = counter = 1
+        print(counter)
